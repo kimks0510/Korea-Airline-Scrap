72bd17d8117cdcc5b144e934572ee703a45213ca
kimks0510 <kimks0510@users.noreply.github.com>
2026-07-20 01:42:28 +0900

    Publish Korean Air report 2026-07-20

--- scripts/Send-KakaoLink.ps1
+++ scripts/Send-KakaoLink.ps1
@@ -7,9 +7,14 @@ $ErrorActionPreference = 'Stop'
 . (Join-Path $PSScriptRoot 'KakaoCommon.ps1')
 
 $url = "$($SiteUrl.TrimEnd('/'))/#$ReportDate"
+# Windows PowerShell 5.1 can misread a BOM-less UTF-8 script. Keep the source
+# text ASCII-only and decode the Korean message explicitly as UTF-8.
+$koreanCodePoints = @(0xB300,0xD55C,0xD56D,0xACF5,0x20,0xC2A4,0xD06C,0xB7A9,0x5D,0xA,0xBAA8,0xBC14,0xC77C,0x20,0xB9AC,0xD3EC,0xD2B8,0xC5D0,0xC11C,0x20,0xC804,0xCCB4,0x20,0xB0B4,0xC6A9,0xC744,0x20,0xD655,0xC778,0xD558,0xC138,0xC694,0x2E)
+$messageFormat = '[{0} ' + (-join ($koreanCodePoints | ForEach-Object { [char]$_ }))
+$messageText = [string]::Format($messageFormat, $ReportDate)
 $template = @{
     object_type = 'text'
-    text = "[$ReportDate 대한항공 스크랩]`n모바일 리포트에서 전체 내용을 확인하세요.`n$url"
+    text = "$messageText`n$url"
     link = @{ web_url = $url; mobile_web_url = $url }
     button_title = '리포트 열기'
 } | ConvertTo-Json -Depth 5 -Compress
